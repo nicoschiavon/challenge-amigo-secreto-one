ca23eaa0f046bc011cdf313ea80ab8d33cae8baa
nicoschiavon <nico.schiavon@gmail.com>
2025-01-26 11:31:49 -0300

    Acutaliza el app.js Implementa funcion mostarAmigos

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
+// Función para agregar un amigo a la lista
 function agregarAmigo() {
     let nombre = document.getElementById('amigo').value;
     if (nombre != "") {
@@ -9,5 +10,16 @@ function agregarAmigo() {
 
     } else {
         alert("Por favor ingrese un nombre.");
+    }
+    mostrarAmigos(amigos);
+}
+
+function mostrarAmigos(amigos) {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+    for (i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        lista.appendChild(li);
     }
 }
